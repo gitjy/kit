cf802be953881a5399dd113203e6a21a7d1faa4c
jiangyong <jiangyong.go@qq.com>
2022-10-20 18:30:44 +0800

    merge

--- xhprof.php
+++ xhprof.php
@@ -1,28 +1,21 @@
 <?php
-//xhprof实例
-// start profiling
-include 'autoload.php';
-if (isset($_GET['xhprofdb'])) {
-	Unit::xhprofdb();
-} else {
-	Unit::xhprof();
+
+function xhprof()
+{
+    xhprof_enable(XHPROF_FLAGS_NO_BUILTINS | XHPROF_FLAGS_CPU | XHPROF_FLAGS_MEMORY, array());
+    register_shutdown_function(function () {
+    $xdata = xhprof_disable();
+    $XHPROF_ROOT = '/Users/siyu/data/www/xhprof';
+    include_once $XHPROF_ROOT . '/xhprof/xhprof_lib/utils/xhprof_lib.php';
+    include_once $XHPROF_ROOT . '/xhprof/xhprof_lib/utils/xhprof_runs.php';
+    //让数据收集程序在后台运行
+    if (function_exists('fastcgi_finish_request')) {
+        fastcgi_finish_request();
+    }
+        $xruns = new XHprofRuns_Default();
+        $name = preg_replace(array("/(\?.*)/", "/[^\w]+/"), array('', '_'), $_SERVER['REQUEST_URI']);
+        if (!$name)
+            $name = 'index';
+        $xruns->save_run($xdata, $name);
+    });
 }
-
-
-function bar($x) {
-  if ($x > 0) {
-    bar($x - 1);
-  }
-}
-
-function foo() {
-  for ($idx = 0; $idx < 5; $idx++) {
-    bar($idx);
-    $x = strlen("abc");
-  }
-}
-
-
-// run program
-foo();
-
